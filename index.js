49bc1afd4f5520591ed72ab21a4a75f5694ce479
priyanshk20 <khandelwalpriyansh85@gmail.com>
2022-09-11 17:10:53 +0530

    added drag and drop upload feature

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const dropZone = document.querySelector('.drop-zone');
+const fileInput = document.getElementById('fileInput');
+const browseBtn = document.querySelector('.browsebtn');
+
+dropZone.addEventListener('dragover',(e)=> {
+
+    e.preventDefault();
+
+    if(!dropZone.classList.contains("dragged")){
+        dropZone.classList.add("dragged");
+    }
+})
+
+dropZone.addEventListener('dragleave',(e)=>{
+    dropZone.classList.remove("dragged");
+})
 
 
+dropZone.addEventListener('drop',(e)=> {
+    e.preventDefault();
+    dropZone.classList.remove("dragged");
+    const files = e.dataTransfer.files;
+    if(files.length){
+        fileInput.files = files;
+    }
+})
+
+
+browseBtn.addEventListener('click',()=>{
+    fileInput.click();
+})
