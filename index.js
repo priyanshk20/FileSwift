f073dda04437f5fb23c65132be40c2645489b994
priyanshk20 <khandelwalpriyansh85@gmail.com>
2022-09-12 16:04:46 +0530

    upload functionality completed through XMLHttpRequest

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const dropZone = document.querySelector('.drop-zone');
 const fileInput = document.getElementById('fileInput');
 const browseBtn = document.querySelector('.browsebtn');
+const host = "https://innshare.herokuapp.com";
+const uploadURL = `${host}/api/files`;
 
 dropZone.addEventListener('dragover',(e)=> {
 
@@ -23,9 +25,28 @@ dropZone.addEventListener('drop',(e)=> {
     if(files.length){
         fileInput.files = files;
     }
+    uploadFile();
 })
 
 
 browseBtn.addEventListener('click',()=>{
     fileInput.click();
 })
+
+fileInput.addEventListener('change',()=> {
+    uploadFile();
+})
+
+const uploadFile = () => {
+file = fileInput.files[0];
+const formData = new FormData();
+formData.append("myfile", file);
+const req = new XMLHttpRequest();
+req.onreadystatechange = () => {
+if(req.readyState === XMLHttpRequest.DONE){
+    console.log(req.responseText);
+}
+}
+req.open("POST",uploadURL);
+req.send(formData);
+}
